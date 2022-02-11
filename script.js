4201a28dc5714fa0311c50d5158a7468442e2531
ctalbot2004 <ctalbot2004@gmail.com>
2022-02-11 11:01:06 -0500

    Update operator buttons

--- script.js
+++ script.js
@@ -1,76 +1,47 @@
-function add (x, y) {
-    if (typeof (x + y) === 'number') return x + y;
-    else return 'Error';
-}
-
-function subtract (x, y) {
-    if (typeof (x - y) === 'number') return x - y;
-    else return 'Error';
-}
-
-function multiply (x, y) {
-    if (typeof (x * y) === 'number') return x * y;
-    else return 'Error';
-}
-
-function divide (x, y) {
-    if (typeof (x / y) === 'number') return x / y;
-    else return 'Error';
-}
-
-function operate (operator, x, y) {
-    if (operator === 'add') {
-        return add(x, y);
-    }
-    else if (operator === 'subtract') {
-        return subtract(x, y);
-    }
-    else if (operator === 'multiply') {
-        return multiply(x, y);
-    }
-    else if (operator === 'divide') {
-        return divide(x, y);
-    }
-    else {
-        return 'Error';
-    }
-}
-
-const display = document.querySelector('.display');
+const displayContainer = document.querySelector('.display');
 const numberButtons = document.querySelectorAll('.number');
 
-let number = 0;
-setNumber(number);
+let lastOperation;
+let total = 0;
+let display = total;
+setDisplay(total);
+let clearOnNextInput = false;
 
-function setNumber (n) {
-    number = n;
-    display.textContent = 0;
+function setDisplay (n) {
+    display = n;
+    showDisplay(n);
 }
 
-function appendNumber (digit) {
-    if (number == 0) {
-        setNumber(digit);
+function showDisplay (text) {
+    displayContainer.textContent = text;
+}
+
+function appendDisplay (digit) {
+    if (clearOnNextInput === true) {
+        setDisplay(digit);
         clearDisplay();
+        clearOnNextInput = false;
     }
-    else number += String(digit);
-    number = Number(number);
-    if (!number) {
+    else display += String(digit);
+    setDisplay(Number(display));
+    if (display != 0 && !display) {
         console.log('Number is falsy!');
-        display.textContent = 'Error';
+        showDisplay('Error');
         return;
     }
-    display.textContent = number;
-    console.log('Number updated to ' + number + ' with a type of ' + typeof number);
+    console.log(display + ' ' + total);
 }
 
-function clearDisplay (zero) {
-    display.textContent = '';
+function clearDisplay () {
+    showDisplay('');
     console.log('Cleared display temporarily!');
 }
 
 const clearButton = document.querySelector('.clear');
 clearButton.addEventListener('click', () => {
-    setNumber(0);
+    setDisplay(0);
+    total = 0;
+    lastOperation = undefined;
     console.log('Cleared display!');
     }
 );
@@ -78,5 +49,62 @@ clearButton.addEventListener('click', () => {
 numberButtons.forEach(button => button.addEventListener('click', handleNumberClick));
 
 function handleNumberClick (e) {
-    appendNumber(Number(this.textContent));
+    appendDisplay(Number(this.textContent));
+}
+
+const opeatorButtons = document.querySelectorAll('.operator');
+
+opeatorButtons.forEach(button => button.addEventListener('click', handleOperatorClick));
+
+function handleOperatorClick (e) {
+    if (!clearOnNextInput) {
+        switch (lastOperation) {
+            case 'add':
+                total += display;
+                break;
+            case 'subtract':
+                total -= display;
+                break;
+            case 'multiply':
+                total *= display;
+                break;
+            case 'divide':
+                total /= display;
+                break;
+            default:
+                total = display;
+        }
+    }
+
+    setDisplay(total);
+    clearOnNextInput = true;
+
+    if (Array.from(this.classList).includes('add')) {
+        lastOperation = 'add';
+    }
+    else if (Array.from(this.classList).includes('subtract')) {
+        lastOperation = 'subtract';
+    }
+    else if (Array.from(this.classList).includes('multiply')) {
+        lastOperation = 'multiply';
+    }
+    else if (Array.from(this.classList).includes('divide')) {
+        lastOperation = 'divide';
+    }
+    else {
+        lastOperation = undefined;
+    }
+    console.log(display + ' ' + total);
+}
+
+const plusMinusButton = document.querySelector('.plus-minus');
+
+plusMinusButton.addEventListener('click', handlePMClick);
+
+function handlePMClick () {
+    setDisplay(display * -1);
+    if (lastOperation === undefined) {
+        total = display;
+    }
+    console.log(display + ' ' + total);
 }
